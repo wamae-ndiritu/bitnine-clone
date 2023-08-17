26d74a0046845602934dd311b4a86dc0a87b566c
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-08-17 13:34:59 +0300

    Design banner info section

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,7 +1,47 @@
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className='cont'>
+      <div className='top-header'>
+        <h6>
+          <span>AG Cloud Express :</span>
+          Experience graph database in a cloud environment for FREE!
+        </h6>
+      </div>
+      <div className='main-header'>
+        <div className='social-row'>
+          <div className='right'>
+            <div className='info'>
+              <div className='search'>
+                <i className='fa fa-search' aria-hidden='true'></i>
+                <i className='fa fa-times d-none' aria-hidden='true'></i>
+              </div>
+              <h6>Contact</h6>
+            </div>
+            <div className='icons'>
+              <span>
+                <i className='fa fa-github' aria-hidden='true'></i>
+              </span>
+              <span>
+                <i className='fa fa-youtube-play' aria-hidden='true'></i>
+              </span>
+              <span>
+                <i className='fa fa-linkedin' aria-hidden='true'></i>
+              </span>
+              <span>
+                <i className='fa fa-facebook' aria-hidden='true'></i>
+              </span>
+              <span>
+                <i className='fa fa-medium' aria-hidden='true'></i>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className='navbar'></div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
