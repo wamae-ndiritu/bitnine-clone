598df218e0ccd378bd489bce05b374cff0518aa2
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-08-19 17:49:35 +0300

    Add agensPackage section

--- src/sections/AgensPackage.jsx
+++ src/sections/AgensPackage.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const AgensPackage = () => {
-  return <div>AgensPackage</div>;
+  return (
+    <div className='package-info'>
+      <div className='package-wrapper'>
+        <img src='/assets/bitnine-img-1.png' alt='...' />
+      </div>
+    </div>
+  );
 };
 
 export default AgensPackage;
