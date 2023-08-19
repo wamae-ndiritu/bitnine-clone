fd37c01139a4905ec9cd1032077944ff89243787
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-08-19 18:13:49 +0300

    Add feature section

--- src/sections/Features.jsx
+++ src/sections/Features.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Features = () => {
-  return <div>Features</div>;
+  return (
+    <div className='features-info'>
+      <h1 className='h1'>Key features</h1>
+      <p>Manage your data with Agens Enterprise Package</p>
+      <p>
+        Essential enterprise features such as high availability and sharding are
+        provided
+      </p>
+      <div className='feature-img'>
+        <img src='/assets/bitnine-img-2.png' about='...' />
+      </div>
+    </div>
+  );
 };
 
 export default Features;
