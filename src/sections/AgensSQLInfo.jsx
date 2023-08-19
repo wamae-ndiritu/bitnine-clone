b35a01999d6e8b140c691b10945d0966c3af22a4
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-08-19 17:28:28 +0300

    Add AgensSQL info section

--- src/sections/AgensSQLInfo.jsx
+++ src/sections/AgensSQLInfo.jsx
@@ -1,7 +1,37 @@
 import React from "react";
 
 const AgensSQLInfo = () => {
-  return <div>AgensSQLInfo</div>;
+  return (
+    <div className='info-cont'>
+      <div className='wrapper'>
+        <div className='info-logo'>
+          <img src='/assets/AgenSQL-Logo.png' alt='...' />
+        </div>
+        <h6 className='h6'>
+          An integration of Bitnine’s DB technology and PG expertise
+        </h6>
+        <p>
+          AgensSQL is an all-new relational DBMS based on PostgreSQL, with years
+          of expertise and knowledge accumulated through database research and
+          development.
+        </p>
+        <p>
+          The enterprise package, along with AgensSQL engine, is an all-in-one
+          solution that ensures the efficiency and scalability of data
+          management.
+        </p>
+        <p>
+          Get AgensSQL now for stable operation and management services at a
+          reduced maintenance cost.
+        </p>
+        <div className='btn-cont'>
+          <button>Contact</button>
+          <button>Brochure</button>
+          <button>Blog</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AgensSQLInfo;
