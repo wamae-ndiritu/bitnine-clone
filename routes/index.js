c106f387a0cd8a8058b7647e16bf1caad4b535ee
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-08-21 17:01:57 +0300

    Refactor register route to use sql db

--- routes/index.js
+++ routes/index.js
@@ -1,36 +1,45 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
+const { db } = require("../configDB");
 const { createUserSchema } = require("../validation");
 
 const router = express.Router();
 
 router.post("/register", async (req, res) => {
-  try {
-    const { username, email, password } = req.body;
-    const { error } = createUserSchema.validate(req.body);
+  const { username, email, password } = req.body;
+  const { error } = createUserSchema.validate(req.body);
 
-    if (error) {
-      return res.status(400).json({ message: error.details[0].message });
-    }
-
-    const existingUser = await User.find({ email });
-    console.log(existingUser);
-    if (existingUser.length > 0) {
-      return res.status(400).json({ mesage: "User already exist!" });
-    }
-    //   Hash the password
-    const hash = await bcrypt.hash(password, 20);
-    const user = await User.create({
-      username,
-      email,
-      password: hash,
-    });
-
-    await user.save();
-    res.status(201).json(user);
-  } catch (err) {
-    console.log(err);
+  if (error) {
+    return res.status(400).json({ message: error.details[0].message });
   }
+
+  const query = "SELECT * FROM `users` WHERE email = ?";
+
+  db.query(query, [email], (err, data) => {
+    if (err) {
+      console.log(err);
+      res.status(500).json({ message: "Internal error occurred!" });
+    } else if (data.length) {
+      res.status(409).json({ message: "User already exists!" });
+    } else {
+      //Hash the password and create a user
+      const salt = bcrypt.genSaltSync(10);
+      const hash = bcrypt.hashSync(password, salt);
+
+      const query =
+        "INSERT INTO `users`(`username`,`email`,`password`) VALUES (?)";
+
+      const values = [username, email, hash];
+
+      db.query(query, [values], (err, data) => {
+        if (err) {
+          console.log(err);
+          res.status(500).json({ message: "Internal error occurred!" });
+        }
+        res.status(201).json({ message: "User created successfully!" });
+      });
+    }
+  });
 });
 
 module.exports = router;
