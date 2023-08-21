8d7e0cee36b29b37f8199da942cd0ebbfbba18d1
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-08-21 16:31:01 +0300

    Verify login details

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const connectDB = require("./connectDB");
+const router = require("./routes");
 
 const PORT = process.env.PORT || 5000;
 
@@ -11,6 +12,8 @@ app.use(express.json());
 app.use(cors());
 
 connectDB();
+
+app.use("/api/v1/users", router);
 
 app.listen(PORT, () => {
   console.log(`Auth backend running on port ${PORT}`);
--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,27 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 const User = require("../models/user");
+const { createUserSchema } = require("../validation");
 
 const router = express.Router();
 
 router.post("/register", async (req, res) => {
   try {
     const { username, email, password } = req.body;
+    const { error } = createUserSchema.validate(req.body);
+
+    if (error) {
+      return res.status(400).json({ message: error.details[0].message });
+    }
+
     const existingUser = await User.find({ email });
-    if (existingUser) {
-      res.status(400).json({ mesage: "User already exist!" });
+    console.log(existingUser);
+    if (existingUser.length > 0) {
+      return res.status(400).json({ mesage: "User already exist!" });
     }
     //   Hash the password
-    const hash = await bcrypt.hashSync(password, 20);
-    const user = await new User({
+    const hash = await bcrypt.hash(password, 20);
+    const user = await User.create({
       username,
       email,
       password: hash,
@@ -25,3 +33,5 @@ router.post("/register", async (req, res) => {
     console.log(err);
   }
 });
+
+module.exports = router;
