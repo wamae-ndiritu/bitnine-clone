c19655c4b7c8fd6c910c47634540010583d3c39c
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-08-22 14:24:59 +0300

    Return true on successful registeration

--- routes/index.js
+++ routes/index.js
@@ -38,7 +38,7 @@ router.post("/register", (req, res) => {
           console.log(err);
           res.status(500).json({ message: "Internal error occurred!" });
         }
-        res.status(201).json({ message: "User created successfully!" });
+        res.status(201).json({ register: true });
       });
     }
   });
