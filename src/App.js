64b6b8699c6f57565af3276ea458da4d38c00133
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-08-22 15:09:59 +0300

    Protect routes to be accessed on login

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Outlet,
+  Navigate,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
+import { useGlobalContext } from "./context/store";
+
+function AuthLayout() {
+  const { userInfo } = useGlobalContext();
+
+  if (userInfo.token) {
+    return <Outlet />;
+  }
+  return <Navigate to={"/login"} />;
+}
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Home />} />
         <Route path='/register' element={<SignUp />} />
         <Route path='/login' element={<SignIn />} />
+        <Route element={<AuthLayout />}>
+          <Route path='/' element={<Home />} />
+        </Route>
       </Routes>
     </Router>
   );
